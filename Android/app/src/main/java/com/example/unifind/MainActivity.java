d1a2b1a670d68a6093bfd9ff39f4e18c478fac90
YerbolNursat <nursat2000ktl@gmail.com>
2019-11-22 17:47:37 +0600

    create ui pages:Login,Universities

--- Android/app/src/main/java/com/example/unifind/MainActivity.java
+++ Android/app/src/main/java/com/example/unifind/MainActivity.java
@@ -18,10 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.universities, R.id.uni_picker, R.id.user_page)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
